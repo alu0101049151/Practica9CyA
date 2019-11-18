359af82bfb2e7332b185fa01cf0308b5793f6a44
Basilio Gómez Navarro <alu0101049151@ull.edu.es>
2019-11-18 00:17:42 +0000

    Created GSymbols Class

---
+++ Src/GSymbols.cpp
@@ -0,0 +1,60 @@
+/**
+ * @Author Basilio Gómez Navarro
+ * @date 17/11/2019
+ * University of La Laguna
+ * Higher School of Engineering and Technology
+ * Computer Ingineering Degree
+ * Grade: 2nd
+ * Practice 9 - Chomsky Normal Form of a Context Free Grammar.
+ * Email: alu101049151@ull.edu.es
+ * GSymbols.h file: GSymbols class. It representes a set of grammatical 
+ *                  symbols, in this case, it can be a set of terminal symbols
+ *                  or a set of non-terminal symbols)
+ *                  This file contains the class implementation. 
+ * References:
+ *                Practice statement:
+ *                https://campusvirtual.ull.es/1920/pluginfile.php/188731/mod_assign/introattachment/0/CYA_1920_Practica_9.pdf?forcedownload=1
+ * Revision history:
+ *                18/11/2019 - Creation (first version) of the code
+ */
+
+
+ #include "GSymbols.h"
+
+
+GSymbols::GSymbols () {}
+
+GSymbols::~GSymbols () {}
+
+/**
+* Inserts the symbol passed as a parameter in the alphabet. 
+*/
+void GSymbols::insertGSymbols (const char symbol) {
+	alphabet_.insert(symbol);
+}
+
+/**
+* @brief returns alphabet.
+*/
+std::set<char> GSymbols::getGSymbols() const {
+  return alphabet_;
+}
+
+/**
+* @brief check if a symbol belongs tot the alphabet.
+* @return true if it belongs or false if not.
+*/
+bool GSymbols::checkIfBelongs(char toCheck) {
+  bool belong = false;
+  for (auto i: alphabet_) {
+    if (i == toCheck)
+      belong = true;
+  }
+  return belong;
+}
+
+GSymbols& GSymbols::operator= (const GSymbols &alphabet) {
+  this->alphabet_ = alphabet.alphabet_;
+  return *this;
+}
+
---
+++ Src/GSymbols.h
@@ -0,0 +1,60 @@
+/**
+ * @Author Basilio Gómez Navarro
+ * @date 17/11/2019
+ * University of La Laguna
+ * Higher School of Engineering and Technology
+ * Computer Ingineering Degree
+ * Grade: 2nd
+ * Practice 9 - Chomsky Normal Form of a Context Free Grammar.
+ * Email: alu101049151@ull.edu.es
+ * GSymbols.h file: GSymbols class. It representes a set of grammatical 
+ *                  symbols, in this case, it can be a set of terminal symbols
+ *                  or a set of non-terminal symbols)
+ *                  This file contains the class definition.
+ * References:
+ *                Practice statement:
+ *                https://campusvirtual.ull.es/1920/pluginfile.php/188731/mod_assign/introattachment/0/CYA_1920_Practica_9.pdf?forcedownload=1
+ * Revision history:
+ *                18/11/2019 - Creation (first version) of the code
+ */
+
+#ifndef PRACTICE9_GSYMBOLS_H
+#define PRACTICE9_GSYMBOLS_H
+
+#include <set>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <stdlib.h>
+
+class GSymbols {
+	
+	public:
+		GSymbols ();
+		~GSymbols ();
+
+		/**
+		* @brief inserts a new symbol into the alphabet.
+		* @param symbol is the new symbol to insert.
+		*/
+		void insertGSymbols (const char symbol);
+
+    /**
+    * @brief returns alphabet.
+    */
+    std::set<char> getGSymbols() const;
+
+    /**
+    * @brief check if a symbol belongs tot the alphabet.
+    * @return true if it belongs or false if not.
+    */
+    bool checkIfBelongs(char toCheck);
+
+    GSymbols& operator= (const GSymbols &alphabet);
+
+	private:
+		std::set<char> alphabet_; //!< Character set representing the DFA's alphabet
+};
+
+#endif //PRACTICE9_GSYMBOLS_H
+
